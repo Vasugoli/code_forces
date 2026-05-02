392d93d025e85e4eed5c50fa63d5a58c9f1b72d3
Vasugoli <golivasu7@gmail.com>
2026-05-02 20:53:14 +0530

    today problem 1873D solved

---
+++ eraser.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+
+		string s;
+		cin >> s;
+
+		int ans = 0;
+
+		for (int i = 0; i < n;) {
+			if (s[i] == 'B') {
+				ans++;
+				i += k;
+			} else {
+				i++;
+			}
+		}
+
+		cout << ans << '\n';
+	}
+
+	return 0;
+}
